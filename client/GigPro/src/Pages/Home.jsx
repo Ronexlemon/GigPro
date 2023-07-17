66398f686cbfd5e2ac70e1df14d009acfd1ef5a2
RonexLemon <ronexondimu@gmail.com>
2023-07-17 09:37:21 +0300

    addhomepage

--- client/GigPro/src/Pages/Home.jsx
+++ client/GigPro/src/Pages/Home.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 const Home =()=>{
     return(
-        <div className="h-screen w- full bg-red-400 ">
+        <div className="min-h-full h-screen w-full bg-black ">
+            <div className="flex justify-center items-center  h-full w-full   ">
+
+                <div className="h-1/2 w-1/2 border-r-8 bg-gray-200 text-red-500">
+                    <h1>dnbfjnbjgfnbjgn</h1>
+
+                </div>
+
+            </div>
 
         </div>
 
