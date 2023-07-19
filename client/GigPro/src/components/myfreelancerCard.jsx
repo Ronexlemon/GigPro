7bde51490e36258750bae625a9a2b0d46be4f86e
RonexLemon <ronexondimu@gmail.com>
2023-07-19 11:56:20 +0300

    implement my freelancers

--- client/GigPro/src/components/myfreelancerCard.jsx
+++ client/GigPro/src/components/myfreelancerCard.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { useState } from "react";
+import { useContractWrite,useContractRead } from "wagmi";
+import { GigProContract } from "../Constant/gigprocontract";
+import gigproAbi from "../ABI/GigPro.json";
+import { useAccount } from 'wagmi'
+
 
 const MyFreelancerCard = () => {
+  const [freelancers,setFreeLancers] = useState([]);
+  const { address, isConnecting, isDisconnected } = useAccount()
+  const { data:myFreelancers, isError, isLoading } = useContractRead({
+    address: GigProContract,
+    abi: gigproAbi,
+    functionName: 'getFreeLancersByOwner',
+    args: [address]
+  })
+  console.log("addressis:,",myFreelancers);
+
   const employess = [
     { address: '0x8878787874827487vdfjdfywetf6f23276r', amount: '4000' },
     { address: '0x8878787874827487vdfjdfywetf6f23276r', amount: '4000' },
@@ -14,18 +30,18 @@ const MyFreelancerCard = () => {
 
   return (
     <>
-      {employess.map((employee, index) => (
+      {myFreelancers.map((employee, index) => (
         <div key={index} className="flex h-full flex-col gap-4 text-white mb-4 border-b border-red-300">
           <div className="flex justify-evenly w-full gap-4 items-center">
             <h3>FreeLancer Address: </h3>
             <span className="text-orange-400">
-            {employee.address}
+            {employee.userAddress}
             </span>
             
           </div>
           <div className="flex  justify-stretch w-full gap-4 items-center">
             <h3>Amount in Cusd: </h3>
-            <span className="text-orange-400">{employee.amount}</span>
+            <span className="text-orange-400">{Number(employee.payAmount)}</span>
             
           </div>
           <div className="flex justify-between items-center text-black">
