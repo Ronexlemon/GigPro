bb948a62385445f5b30079bfb2868f69043b30a6
RonexLemon <ronexondimu@gmail.com>
2023-07-19 11:09:54 +0300

    add walletConnect

---
+++ client/GigPro/src/components/navBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = ()=>{
+    return(
+        <div className="h-10 w-full flex justify-between items-center text-white">
+<div>
+<h2>GigPro</h2>
+</div>
+<button className=" justify-center bg-gray-300 text-black rounded gap-10  text-sm ">
+    Manage
+</button>
+        </div>
+
+    )
+}
+export default Navbar;
