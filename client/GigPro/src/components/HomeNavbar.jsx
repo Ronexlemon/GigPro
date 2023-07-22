32961c8ed64cdcb9bb0abc1696e2b61e4acbd5a4
RonexLemon <ronexondimu@gmail.com>
2023-07-22 18:49:49 +0300

    add designs

--- client/GigPro/src/components/HomeNavbar.jsx
+++ client/GigPro/src/components/HomeNavbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const HomeNavbar = ()=>{
+    const navigate = useNavigate();
+
     return(
-        <div className="h-10 w-full flex justify-between items-center text-white">
+        <div className="h-10 w-full flex justify-between items-center text-white p-10">
 <div>
 <h2>GigPro</h2>
 </div>
-<button className=" justify-center bg-gray-300 text-black rounded gap-10  text-sm ">
+<button onClick={()=>{navigate("/add")}} className=" justify-center bg-gray-300 h-8 rounded gap-10  text-sm text-orange-400">
     Manage
 </button>
         </div>
