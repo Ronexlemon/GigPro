cf7407e520dff8f8f20987a398f7b03b55c60503
RonexLemon <ronexondimu@gmail.com>
2023-07-23 20:51:18 +0300

    remove user

--- client/GigPro/src/components/myfreelancerCard.jsx
+++ client/GigPro/src/components/myfreelancerCard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useContractWrite,useContractRead } from "wagmi";
 import { GigProContract } from "../Constant/gigprocontract";
 import gigproAbi from "../ABI/GigPro.json";
@@ -9,7 +9,9 @@ import { useAccount } from 'wagmi'
 const MyFreelancerCard = () => {
   const [freelancers,setFreeLancers] = useState([]);
   const [freeLancerAddress,setFreelancerAddress] = useState();
+  const [deleteuser,setDeleteUser] = useState(true);
   const { address, isConnecting, isDisconnected } = useAccount()
+  
   const { data:myFreelancers, isError, isLoading } = useContractRead({
     address: GigProContract,
     abi: gigproAbi,
@@ -24,12 +26,10 @@ const MyFreelancerCard = () => {
     functionName:"removeFreeLancer",
     args:[freeLancerAddress,address]
   })
-const removeFreeeLancer = async(freeaddress)=>{
-  //setFreelancerAddress(freeaddress);
-  console.log("the free",freeaddress);
+const removeFreeeLancer = async()=>{
+  
   try{
-
-    
+   
      
       if(freeLancerAddress != undefined){
         await removeFreeLancer();
@@ -53,11 +53,11 @@ const removeFreeeLancer = async(freeaddress)=>{
     { address: '0x8878787874827487vdfjdfywetf6f23276r', amount: '4000' },
     { address: '0x8878787874827487vdfjdfywetf6f23276r', amount: '4000' },
   ];
-
+ 
   return (
     <>
       {myFreelancers.map((employee, index) => (
-        <div key={index} className="flex h-full flex-col gap-4 text-white mb-4 border-b border-red-300">
+        <div key={index} className="flex h-full flex-col gap-2 text-white mb-0 border-b border-red-300">
           <div className="flex justify-evenly w-full gap-4 items-center">
             <h3>FreeLancer Address: </h3>
             <span className="text-orange-400">
@@ -65,16 +65,19 @@ const removeFreeeLancer = async(freeaddress)=>{
             </span>
             
           </div>
-          <div className="flex  justify-stretch w-full gap-4 items-center">
+          <div className="flex  justify-stretch w-full gap-2 items-center">
             <h3>Amount in Cusd: </h3>
             <span className="text-orange-400">{Number(employee.payAmount)}</span>
             
           </div>
           <div className="flex justify-between items-center text-black">
-          
-            <button onClick={()=>{removeFreeeLancer(employee.userAddress)}} className="inline-flex p-2 justify-center items-center w-100 rounded-full text-red-500">
+            {deleteuser? <button onClick={()=>{setFreelancerAddress(employee.userAddress);setDeleteUser(false)}} className="inline-flex pl-2 justify-center items-center w-100 rounded-full text-red-500">
               End Contract
-            </button>
+            </button>: <button onClick={()=>{removeFreeeLancer()}} className="inline-flex pl-2 justify-center items-center w-100 rounded-full text-yellow-500">
+              Approve
+            </button>}
+          
+           
             
             
           </div>
