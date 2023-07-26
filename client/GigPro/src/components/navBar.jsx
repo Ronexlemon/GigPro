b92e48a47abd4bb351d7faed06bad407c60f8c5c
RonexLemon <ronexondimu@gmail.com>
2023-07-26 22:54:41 +0300

    button theme

--- client/GigPro/src/components/navBar.jsx
+++ client/GigPro/src/components/navBar.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { Web3Button } from "@web3modal/react";
+import { useNavigate } from "react-router-dom";
 const Navbar = ()=>{
+    const navigate = useNavigate();
     return(
-        <div className="h-10 w-full flex justify-between items-center text-white ">
-<div>
-<h2>GigPro</h2>
+        <div className="h-10 w-full flex justify-between items-center text-white  ">
+<div className=" animate-pulse p-5">
+<button onClick={()=>{navigate("/home")}}>GigPro</button>
 </div>
-<Web3Button/>
+<div className="p-5 pt-10">
+<Web3Button themeVariables={{
+    '--w3m-font-family': 'Roboto, sans-serif',
+    '--w3m-accent-color': '#FF0000.'
+  }}/>
+</div>
+
         </div>
 
     )
